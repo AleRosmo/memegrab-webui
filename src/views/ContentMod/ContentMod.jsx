5d8311a4f17f55b8bbc1cff7a2cd44a48272c468
Revengeic3 <onair95@gmail.com>
2023-04-03 20:19:33 +0200

    Fix carousel

--- src/views/ContentMod/ContentMod.jsx
+++ src/views/ContentMod/ContentMod.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { AiFillCheckCircle } from "react-icons/ai";
 import {
 	BsFillArrowLeftCircleFill,
@@ -30,6 +30,37 @@ export default function ContentMod({ info }) {
 	// 	});
 	// };
 
+	const scrollLeft = () => {
+		let scrollContainer = document.getElementById("scroll-container");
+		console.log(scrollContainer.scrollLeft);
+		scrollContainer.scrollLeft -= 750;
+		console.log(scrollContainer.scrollLeft);
+	};
+
+	const scrollRight = () => {
+		let scrollContainer = document.getElementById("scroll-container");
+		console.log(scrollContainer.scrollLeft);
+		scrollContainer.scrollLeft += 750;
+		console.log(scrollContainer.scrollLeft);
+	};
+
+	useEffect(() => {
+		loadContent();
+		const keyboardEvent = (e) => {
+			if (e.key === "ArrowRight") {
+				// nextPost();
+			}
+			if (e.key === "ArrowLeft") {
+				// prevPost();
+			}
+		};
+		window.addEventListener("keydown", keyboardEvent, false);
+
+		return () => {
+			window.removeEventListener("keydown", keyboardEvent);
+		};
+	}, []);
+
 	const loadContent = () => {
 		axios.get(info.url, { withCredentials: true }).then((response) => {
 			const dataMap = response.data.map((item) => {
@@ -37,37 +68,32 @@ export default function ContentMod({ info }) {
 				// const exists = checkImgExist(url);
 				// if (exists) {
 				return (
-					<div key={item.id} id={item.id} className='flex flex-row snap-center'>
+					<li key={item.id} id={item.id} className='snap-center block'>
 						<img key={item.file_name} className='max-w-xl' src={url} />
-					</div>
+					</li>
 				);
 			});
 			setData(dataMap);
 		});
 	};
 
-	useEffect(() => {
-		loadContent();
-		window.addEventListener("keydown", (e) => {
-			if (e.key === "ArrowRight") {
-				// nextPost();
-			}
-			if (e.key === "ArrowLeft") {
-				// prevPost();
-			}
-		});
-	}, []);
-
 	return (
-		<div className='w-full h-screen flex flex-col'>
-			<div className='flex flex-row items-center justify-center overflow-scroll snap-x snap-mandatory gap-20'>
+		<div className='w-full h-screen flex flex-col overflow-hidden'>
+			{/* Carousel wrapper */}
+			<div
+				className='relative flex flex-row w-full h-full overflow-hidden rounded-lg'
+				id='scroll-container'>
 				{data}
 			</div>
+			{/* */}
+			<div
+				className='flex flex-row items-center justify-center \
+				overflow-scroll snap-x snap-mandatory'></div>
 			<div className='mb-auto' />
 			<div className='relative w-full'>
 				<Container>
 					<div className='flex flex-row items-center justify-around'>
-						<button>
+						<button onClick={scrollLeft}>
 							<BsFillArrowLeftCircleFill color='white' size='24' />
 						</button>
 						<button>
@@ -76,7 +102,7 @@ export default function ContentMod({ info }) {
 						<button>
 							<AiFillCheckCircle color='white' size='24' />
 						</button>
-						<button>
+						<button onClick={scrollRight}>
 							<BsFillArrowRightCircleFill color='white' size='24' />
 						</button>
 					</div>
